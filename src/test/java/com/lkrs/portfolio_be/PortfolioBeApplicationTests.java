c4d8986b106798943cb07d4e4ab00add3f93d8e0
Nessumsar <lukasrasmusseen@gmail.com>
2025-04-07 14:49:00 +0200

    Init

---
+++ src/test/java/com/lkrs/portfolio_be/PortfolioBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lkrs.portfolio_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
