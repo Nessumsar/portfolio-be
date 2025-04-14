8600f580d9214f539bf8b15eeabc1b725125fe85
Nessumsar <lukasrasmusseen@gmail.com>
2025-04-14 18:33:25 +0200

    JsonAlias for Jackson to map to POJO

--- src/main/java/com/lkrs/portfoliobe/model/Repository.java
+++ src/main/java/com/lkrs/portfoliobe/model/Repository.java
@@ -1,15 +1,21 @@
 package com.lkrs.portfoliobe.model;
 
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
 
 import java.util.Date;
 
 @Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Repository {
     int id;
     String name;
+    @JsonAlias({"html_url", "web_url"})
     String url;
+    @JsonAlias({"updated_at", "last_activity_at"})
     Date lastUpdated;
     String description;
+    String language;
     Platform platform;
 }
