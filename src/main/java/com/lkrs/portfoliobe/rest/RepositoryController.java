a2cab52092688b3cece8f81eb37ed6d159d798d9
Nessumsar <lukasrasmusseen@gmail.com>
2025-04-16 14:23:55 +0200

    returning ResponseEntity in controller

--- src/main/java/com/lkrs/portfoliobe/rest/RepositoryController.java
+++ src/main/java/com/lkrs/portfoliobe/rest/RepositoryController.java
@@ -1,9 +1,12 @@
 package com.lkrs.portfoliobe.rest;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.lkrs.portfoliobe.model.Repository;
 import com.lkrs.portfoliobe.service.RepositoryService;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
@@ -18,10 +21,12 @@ public class RepositoryController {
     }
 
     @GetMapping("/repository")
-    public List<Repository> getAll() throws JsonProcessingException {
-
-
-        return repositoryService.getRepositories();
+    public ResponseEntity<?> getAll() {
+        List<Repository> result = repositoryService.getRepositories();
+        if (result.isEmpty()) {
+            return new ResponseEntity<>("No repositories found.", HttpStatus.NOT_FOUND);
+        }
+        return ResponseEntity.ok(result);
     }
 
 }
