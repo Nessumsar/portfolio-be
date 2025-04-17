71c43f6349bf80313793de0d5a4e9872d532c674
Nessumsar <lukasrasmusseen@gmail.com>
2025-04-17 16:55:08 +0200

    Fetching data with access tokens

--- src/main/java/com/lkrs/portfoliobe/service/RepositoryService.java
+++ src/main/java/com/lkrs/portfoliobe/service/RepositoryService.java
@@ -6,8 +6,8 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.lkrs.portfoliobe.model.Platform;
 import com.lkrs.portfoliobe.model.Repository;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -20,8 +20,16 @@ import java.util.List;
 @Service
 @Slf4j
 public class RepositoryService {
-    private final String GITHUB_URL = "https://api.github.com/users/Nessumsar/repos";
-    private final String GITLAB_URL = "https://gitlab.com/api/v4/users/Nessumsar/projects";
+
+    @Value("${github.repo.url}")
+    private String GITHUB_URL;
+    @Value("${gitlab.repo.url}")
+    private String GITLAB_URL;
+    @Value("${github.access.token}")
+    private String GITHUB_TOKEN;
+    @Value("${gitlab.access.token}")
+    private String GITLAB_TOKEN;
+
     private final ObjectMapper objectMapper;
 
     public RepositoryService(ObjectMapper objectMapper) {
@@ -59,7 +67,8 @@ public class RepositoryService {
         RestTemplate restTemplate = new RestTemplate();
 
         String url = platform.equals(Platform.GITHUB) ? GITHUB_URL : GITLAB_URL;
-        ResponseEntity<String> apiResponse = restTemplate.getForEntity(url, String.class);
+        HttpEntity<String> request = new HttpEntity<>(createHeaders(platform));
+        ResponseEntity<String> apiResponse = restTemplate.exchange(url, HttpMethod.GET, request, String.class);
         if (apiResponse.getStatusCode() != HttpStatus.OK) {
             log.warn("ApiResponse code is not 200: {}", apiResponse.getStatusCode());
             return new ArrayList<>();
@@ -93,5 +102,19 @@ public class RepositoryService {
                 .sorted(Comparator.comparing(Repository::getLastUpdated).reversed())
                 .filter(repository -> repository.getLastUpdated().isAfter(oneYearAgo))
                 .toList();
+    }
+
+    private HttpHeaders createHeaders(Platform platform) {
+        if (platform.equals(Platform.GITHUB)) {
+            return new HttpHeaders() {{
+                set("Content-Type", "application/json");
+                set("Authorization", "Bearer "+GITHUB_TOKEN);
+            }};
+        } else if (platform.equals(Platform.GITLAB)) {
+            return new HttpHeaders() {{
+                set("Content-Type", "application/json");
+                set("Authorization", "Bearer "+GITLAB_TOKEN);
+            }};
+        } else return null;
     }
 }
